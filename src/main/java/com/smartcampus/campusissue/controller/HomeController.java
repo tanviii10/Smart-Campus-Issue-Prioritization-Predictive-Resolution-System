7c000504f80c536b62345eddef4189329a7ab663
Tanvi jagtap <tanvijagtap910@gmail.com>
2026-06-04 17:16:38 +0530

    fix website

---
+++ src/main/java/com/smartcampus/campusissue/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.smartcampus.campusissue.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+    public String home() {
+        return "redirect:/login.html";
+    }
+}
